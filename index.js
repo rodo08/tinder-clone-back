e4ccaa9e61bc31f2f5f6e9d8705fe8a509880587
rodo08 <rodrigo.rosalesmoya@gmail.com>
2024-07-03 10:54:06 +0200

    Add match endpoint

--- index.js
+++ index.js
@@ -48,9 +48,7 @@ app.post("/signup", async (req, res) => {
       expiresIn: 60 * 24,
     });
 
-    res
-      .status(201)
-      .json({ token, userId: generatedUserId, email: sanitizedEmail });
+    res.status(201).json({ token, userId: generatedUserId });
   } catch (error) {
     console.log(error);
   }
@@ -76,7 +74,7 @@ app.post("/login", async (req, res) => {
       const token = jwt.sign(user, email, {
         expiresIn: 60 * 24,
       });
-      res.status(201).json({ token, userId: user.user_id, email });
+      res.status(201).json({ token, userId: user.user_id });
     }
     res.status(400).send("Invalid credentials");
   } catch (error) {
@@ -84,16 +82,92 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.get("/users", async (req, res) => {
+app.get("/user", async (req, res) => {
   const client = new MongoClient(uri);
+  const userId = req.query.userId;
+
+  console.log("userId", userId);
 
   try {
     await client.connect();
     const database = client.db(process.env.DB_NAME);
     const users = database.collection("users");
+    const query = { user_id: userId };
+    const user = await users.findOne(query);
+    res.send(user);
+  } finally {
+    await client.close();
+  }
+});
 
-    const returnedUsers = await users.find().toArray();
-    res.send(returnedUsers);
+app.get("/gendered-users", async (req, res) => {
+  const client = new MongoClient(uri);
+  const gender = req.query.gender;
+
+  console.log("gender", gender);
+
+  try {
+    await client.connect();
+    const database = client.db(process.env.DB_NAME);
+    const users = database.collection("users");
+    const query = { gender_identity: { $eq: gender } };
+    const foundUsers = await users.find(query).toArray();
+
+    res.send(foundUsers);
+  } finally {
+    await client.close();
+  }
+});
+
+app.put("/user", async (req, res) => {
+  const client = new MongoClient(uri);
+  const formData = req.body.formData;
+
+  try {
+    await client.connect();
+    const database = client.db("app-data");
+    const users = database.collection("users");
+
+    const query = { user_id: formData.user_id };
+
+    const updateDocument = {
+      $set: {
+        first_name: formData.first_name,
+        dob_day: formData.dob_day,
+        dob_month: formData.dob_month,
+        dob_year: formData.dob_year,
+        show_gender: formData.show_gender,
+        gender_identity: formData.gender_identity,
+        gender_interest: formData.gender_interest,
+        url: formData.url,
+        about: formData.about,
+        matches: formData.matches,
+      },
+    };
+
+    const insertedUser = await users.updateOne(query, updateDocument);
+
+    res.json(insertedUser);
+  } finally {
+    await client.close();
+  }
+});
+
+app.put("/addmatch", async (req, res) => {
+  const client = new MongoClient(uri);
+  const { userId, matchedUserId } = req.body;
+
+  try {
+    await client.connect();
+    const database = client.db("app-data");
+    const users = database.collection("users");
+
+    const query = { user_id: userId };
+    const updateDocument = {
+      $push: { matches: { user_id: matchedUserId } },
+    };
+    const user = await users.updateOne(query, updateDocument);
+    res.send(user);
   } finally {
     await client.close();
   }
